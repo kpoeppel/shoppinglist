78c3dff3e13e7e1bc2f01c0b12b063a8c11fe1d3
ne0os <u32z45u89z2>
2020-03-21 13:19:49 +0100

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='shoppinglist',
+    version='0.1',
+    packages=['django', 'django-allauth', 'django-appconf', 'django-compressor', 'django-htmlmin', 'django-material',
+              'django-money',
+              'django-phonenumber-field', 'django-registration', 'django-sass-processor', 'django-user-accounts',
+              'django-channels', 'django-user-agents',
+              'redis', 'channels_redis', 'asgiref', 'docupy', 'six'],
+    url='',
+    license='',
+    author='wirvsvirus',
+    author_email='',
+    description=''
+)
